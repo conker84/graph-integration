3c7bdd492b7ab4e40e15b54f19ec4ea7dfe0fc02
Andrea Santurbano <santand@gmail.com>
2021-03-05 14:56:26 +0100

    export data classes in javascript

--- src/commonMain/kotlin/org/neo4j/graph_integration/Entity.kt
+++ src/commonMain/kotlin/org/neo4j/graph_integration/Entity.kt
@@ -1,3 +1,6 @@
 package org.neo4j.graph_integration
 
+import kotlin.js.JsExport
+
+@JsExport
 data class Entity<KEY, VALUE>(val key: KEY? = null, val value: VALUE?)
--- src/commonMain/kotlin/org/neo4j/graph_integration/Event.kt
+++ src/commonMain/kotlin/org/neo4j/graph_integration/Event.kt
@@ -1,7 +1,12 @@
 package org.neo4j.graph_integration
 
+import kotlin.js.JsExport
+
+@JsExport
 data class InvalidEvent<EVT>(val error: Throwable? = null, val event: EVT, val meta: Map<String, Any> = emptyMap())
 
+@JsExport
 data class Event<EVT>(val query: String, val events: List<EVT>)
 
+@JsExport
 data class IngestionEvent<EVT>(val events: List<Event<EVT>>, val invalidEvents: List<InvalidEvent<EVT>> = emptyList())
