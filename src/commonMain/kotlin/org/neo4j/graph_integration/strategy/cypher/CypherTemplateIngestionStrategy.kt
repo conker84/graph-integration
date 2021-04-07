acc6fb430e4d9a009953dc506af4d52aa7a75f2a
Andrea Santurbano <santand@gmail.com>
2021-04-07 12:44:31 +0200

    removed trasformation function

--- src/commonMain/kotlin/org/neo4j/graph_integration/strategy/cypher/CypherTemplateIngestionStrategy.kt
+++ src/commonMain/kotlin/org/neo4j/graph_integration/strategy/cypher/CypherTemplateIngestionStrategy.kt
@@ -6,11 +6,10 @@ import org.neo4j.graph_integration.IngestionEvent
 import org.neo4j.graph_integration.IngestionStrategy
 import org.neo4j.graph_integration.utils.IngestionUtils
 
-class CypherTemplateIngestionStrategy<KEY, VALUE>(query: String,
-                                                       private val transformationFunction: (Entity<KEY, VALUE>) -> Map<String, Any>? = { it.value as? Map<String, Any> }): IngestionStrategy<KEY, VALUE>() {
+class CypherTemplateIngestionStrategy<KEY, VALUE>(query: String): IngestionStrategy<KEY, VALUE>() {
     private val fullQuery = "${IngestionUtils.UNWIND} $query"
     override fun mergeNodeEvents(events: Collection<Entity<KEY, VALUE>>): IngestionEvent {
-        return IngestionEvent(listOf(Event(fullQuery, events.mapNotNull(transformationFunction).toList())))
+        return IngestionEvent(listOf(Event(fullQuery, events.mapNotNull{ it.value as? Map<String, Any> }.toList())))
     }
 
     override fun deleteNodeEvents(events: Collection<Entity<KEY, VALUE>>): IngestionEvent = IngestionEvent(emptyList())
